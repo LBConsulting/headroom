124aead8c5e1e820884e47bd809c1274fe718fb2
Faris Chebib <octaflop@gmail.com>
2012-05-02 22:53:38 -0700

    adding the forms

--- headroom/app.py
+++ headroom/app.py
@@ -1,20 +1,21 @@
-from bottle import route, run, view, static_file
+from bottle import route, run, view, static_file, post, get, redirect
 from bottle import jinja2_template as template
-from db import slideload
+from db import slideload, Slide
 import simplejson as json
 from settings import STATIC_URL, CONFIG_ROOT
+from forms import DemographicsForm
 
 import os
 
-# Constants on initialization
+# DB Constants on initialization
+s = Slide()
 
 ## logging functions
 # Static
 
 @route('/')
 def index():
-    slides = slideload()
-    slide = slides['slides']
+    slide = s.by_weight()[0]
     ret = dict(slide=slide, config=dict(STATIC_URL=STATIC_URL))
     print ret
     return template("slide.html", page=ret)
@@ -25,6 +26,25 @@ def server_static(filename):
     return static_file(filename, root=STATIC_ROOT)
 
 # Dynamic
+
+@get('/input')
+def posted_slide():
+    ret = {}
+    ## get this query into db
+    for slide in s.objects():
+        if slide['kind'] == 'InputSlide':
+            ret = slide
+    next_slide_id = "testing"
+    next_slide = "/slide/%s" % next_slide_id
+    redirect(next_slide)
+
+@post('/input')
+def posted_input():
+    form = DemographicsForm(request.POST)
+    if form.validate():
+        return "Posted month: %s" % form.birth_month.data
+    else:
+        return "Error..."
 
 @route('/slide/<slide_id>')
 def slide(slide_id="intro"):
--- headroom/db.py
+++ headroom/db.py
@@ -28,6 +28,3 @@ class Slide():
     def by_weight(self):
         return sorted(self.slides, key=lambda slide: slide['weight'])
 
-
-
-
