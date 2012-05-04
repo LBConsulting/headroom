ecd92914af1cdb5cad61e80e9b57f40fdba59d46
Faris Chebib <octaflop@gmail.com>
2012-05-04 13:13:40 -0700

    adding test examples and proper writer

--- headroom/db.py
+++ headroom/db.py
@@ -5,25 +5,57 @@ import os
 from operator import itemgetter
 from settings import CONFIG_ROOT, SLIDES_FILE
 
+SLIDES_FILE = 'testing-3.json'
+
 # DBUTILS (MAY BE MOVED)
 # may be needed
 
 ## from __future__ import with
 
+def jsonfileload(config):
+    """
+    Keeping the kwarg for quick loads
+    """
+    retpath = os.path.join(CONFIG_ROOT, config)
+    with open(retpath, 'r') as retfile:
+        retjson = retfile.read()
+    ret = json.loads(retjson)
+    # Just get the slides, not the _doc
+    return ret
+
+def jsonfilewrite(writein, config):
+    """
+    writes and entire key into one file
+    """
+    retpath = os.path.join(CONFIG_ROOT, config)
+    try:
+        jsontowrite = json.dumps(writein)
+    except:
+        print "ERROR Dumping JSON"
+        return False
+    try:
+        with open(retpath, 'w') as jsonfile:
+            jsonfile.write(jsontowrite)
+        jsonfile.close()
+        return True
+    except:
+        print "ERROR Writing File"
+        return False
+
 
 class Model(object):
     def __init__(self, **kwargs):
         self.slidesfile = SLIDES_FILE
-        self.slides = self._load(slidesfile=self.slidesfile)
+        self.slides = self._load()
 
-    def _load(slidesfile=SLIDES_FILE):
+    def _load():
         """
         Keeping the kwarg for quick loads
         """
-        retpath = os.path.join(CONFIG_ROOT, slidesfile)
+        retpath = os.path.join(CONFIG_ROOT, self.slidesfile)
         with open(retpath, 'r') as retfile:
             retjson = retfile.read()
-        ret = json.loads(slidesjson)
+        ret = json.loads(retjson)
         # Just get the slides, not the _doc
         return ret
 
