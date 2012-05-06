fbfa3493d34b555e8cd63e732086b0d89a650aed
Faris Chebib <octaflop@gmail.com>
2012-05-06 13:38:32 -0700

    adding new db read-write models and moving names around to make more sense

--- headroom/settings.py
+++ headroom/settings.py
@@ -3,9 +3,9 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CONFIG_ROOT = os.path.join(BASE_DIR, 'config')
+CONFIG_FILE = "slides.json"
 STATIC_URL = "/static/"
 # In the STATIC_ROOT
-SLIDES_FILE = "slides.json"
 # The written database. Do not store in version control
 DBSUFFIX = "headroom.jsondb"
 DBFOLDER = "db"
